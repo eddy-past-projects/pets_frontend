45e2660e8de7c232488d114b64b8a5263c4b7d9b
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2019-10-15 21:27:46 -0400

    add semantic

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,12 +1,17 @@
 import React from 'react'
 // import { Card, Image, Icon, Button } from 'semantic-ui-react'
+// import { Link } from 'react-router-dom'
+import { Image} from 'semantic-ui-react'
+
+
+
 
 
 const PetCard = ( {pet} ) => {
   return (
 
       <div className='pet-card' data-action='this-pet' data-id={pet.id} >
-        <img scr={pet.image}  alt='' className='pet-image' />
+        <Image src={pet.image}  alt='' className='pet-image' />
         <h3>name: {pet.name}</h3>
         <h5>type of pet: {pet.kind}< br/>
         needs to be fed: {String(pet.hungry)}</h5>
