71953693ae5bef27ab87556ccb3ba48a2492fc97
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-01-23 22:19:28 -0500

    cors

--- src/actions/currentUserAction.js
+++ src/actions/currentUserAction.js
@@ -44,6 +44,7 @@ export const getCurrentUser = () => {
     return fetch("https://fast-waters-11750.herokuapp.com/get_current_user", {
       credentials: "include",
       method: "GET",
+      mode: 'no-cors',
         headers: {
           'Access-Control-Allow-Origin': 'https://fast-waters-11750.herokuapp.com',
           'Content-Type': 'application/json',
