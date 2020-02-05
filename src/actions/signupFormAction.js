6ddac9213a77cbbea50f1a771e67e9fe117dc1ce
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-02-04 19:53:59 -0500

    added h to ttp

--- src/actions/signupFormAction.js
+++ src/actions/signupFormAction.js
@@ -18,7 +18,7 @@ export const signup = (credentials, history) => {
       const userInfo = {
         user: credentials
       }
-      return fetch("ttps://fast-waters-11750.herokuapp.com/signup", {
+      return fetch("https://fast-waters-11750.herokuapp.com/signup", {
         credentials: "include",
         method: "POST",
         headers: {
